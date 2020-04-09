5a260e5255a0a3b2002e47534201e3f82ebacd3e
Peter Harrison <blackriver@simiya.com>
2020-04-09 10:29:15 -0700

    Added navigation

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -16,12 +16,14 @@ class Screen0 extends StatelessWidget {
               child: Text('Go To Screen 1'),
               onPressed: () {
                 //Navigate to Screen 1
+                Navigator.pushNamed(context, '/second');
               },
             ),
             RaisedButton(
               color: Colors.blue,
               child: Text('Go To Screen 2'),
               onPressed: () {
+                Navigator.pushNamed(context, '/second');
                 //Navigate to Screen 2
               },
             ),
